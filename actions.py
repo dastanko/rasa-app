e8257c8849c7311dddb55291a44e13509e050a35
Dastan Kozhomuratov <dastan.kozhomuratov@gmail.com>
2019-01-19 14:40:00 +0600

    added fallback action

--- actions.py
+++ actions.py
@@ -4,9 +4,10 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
-import logging
-import requests
 import json
+import logging
+
+import requests
 from rasa_core_sdk import Action
 
 logger = logging.getLogger(__name__)
@@ -22,4 +23,15 @@ class ActionJoke(Action):
         request = json.loads(requests.get('https://api.chucknorris.io/jokes/random').text)  # make an api call
         joke = request['value']  # extract a joke from returned json response
         dispatcher.utter_message(joke)  # send the message back to the user
+        return []
+
+
+class FallbackAction(Action):
+    def name(self):
+        # define the name of the action which can then be included in training stories
+        return "fallback_action"
+
+    def run(self, dispatcher, tracker, domain):
+        # what your action should do
+        dispatcher.utter_message("Sorry, I don't understand you")  # send the message back to the user
         return []
